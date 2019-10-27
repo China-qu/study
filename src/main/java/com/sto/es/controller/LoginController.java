a6dcf4ecbf5199efc1107a175974a0fd3b69e949
zhouqu <573708366@qq.com>
2019-10-27 15:40:29 +0800

    CV

--- src/main/java/com/sto/es/controller/LoginController.java
+++ src/main/java/com/sto/es/controller/LoginController.java
@@ -1,12 +1,19 @@
 package com.sto.es.controller;
 
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Component
-public class LoginController {
+@SuppressWarnings("deprecation")
+@Configuration
+public class LoginController extends WebMvcConfigurerAdapter {
 	
-	public String login() {
-		return "/login";
-	}
+	@Override
+   public void addViewControllers( ViewControllerRegistry registry ) {
+       registry.addViewController( "/" ).setViewName( "forward:/index.html" );
+       registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
+       super.addViewControllers( registry );
+   }
 
 }
