68cc0fcb4e61c7c1753bf3455e977a87e06af2a4
zhouqu <573708366@qq.com>
2019-10-27 15:29:13 +0800

    s

--- src/main/java/com/sto/es/controller/LoginController.java
+++ src/main/java/com/sto/es/controller/LoginController.java
@@ -1,5 +1,12 @@
 package com.sto.es.controller;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class LoginController {
+	
+	public String login() {
+		return "/login";
+	}
 
 }
